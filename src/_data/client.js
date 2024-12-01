0c48d6f1cb7546b191a87b45450d41e5fea114fe
andrewbramwell <andy@voucherstore.io>
2024-12-01 14:21:17 +0000

    canocial

--- src/_data/client.js
+++ src/_data/client.js
@@ -20,5 +20,5 @@ module.exports = {
         "googleReview": "",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.falkirkthaimassage.co.uk"
+    "domain": "https://falkirkthaimassage.co.uk"
 };
